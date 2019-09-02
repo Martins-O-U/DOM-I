9d54bad07859d5f008ba4aac32537bcdfbccfe13
Martins-O-U <martinsonyedikachi@gmail.com>
2019-09-02 15:31:18 +0100

    First commit on the Dom manipulation, working on the main content

--- js/index.js
+++ js/index.js
@@ -40,3 +40,38 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let nav1 = document.querySelector('nav a:nth-child(1)');
+nav1.textContent = "Services";
+nav1.setAttribute("id", "nav-item-1");
+
+let nav2 = document.querySelector('nav a:nth-child(2)');
+nav2.textContent = "Product";
+nav2.setAttribute("id", "nav-item-2");
+
+let nav3 = document.querySelector('nav a:nth-child(3)');
+nav3.textContent = "Vision";
+nav3.setAttribute("id", "nav-item-3");
+
+let nav4 = document.querySelector('nav a:nth-child(4)');
+nav4.textContent = "Features";
+nav4.setAttribute("id", "nav-item-4");
+
+let nav5 = document.querySelector('nav a:nth-child(5)');
+nav5.textContent = "About";
+nav5.setAttribute("id", "nav-item-5");
+
+let nav6 = document.querySelector('nav a:nth-child(6)');
+nav6.textContent = "Contact";
+nav6.setAttribute("id", "nav-item-6");
+
+let h123= document.getElementsByTagName("h1")[0];
+h123.innerText = "DOM\n Is \n Awesome"
+
+// let button = document.getElementsByTagName('button');
+// button.textContent = "Get Started"
+// console.log(button);
+
+let topImg = document.querySelector("#cta-img");
+topImg.setAttribute("src","img/header-img.png");
+console.log(topImg);
